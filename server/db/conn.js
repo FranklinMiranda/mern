6b123229d306193303b36b2211fd7d29c0bf9451
FranklinMiranda <fdm61344@gmail.com>
2023-03-06 12:24:32 -0500

    Cm

--- server/db/conn.js
+++ server/db/conn.js
@@ -9,15 +9,18 @@ var _db;
 
 module.exports = {
     connectToServer: function (callback) {
-        client.connect(function (err, db) {
-            // Verify we got a good "db" object
-            if (db) 
-            {
-                _db = db.db("employees");
-                console.log("Successfully connected to MongoDB.");
-            }
-            return callback(err);
-        });
+        
+        _db = client.db('employees')
+
+        // client.connect(function (err, db) {
+        //     // Verify we got a good "db" object
+        //     if (db) 
+        //     {
+        //         _db = db.db("employees");
+        //         console.log("Successfully connected to MongoDB.");
+        //     }
+        //     return callback(err);
+        // });
     },
     
     getDb: function () {
